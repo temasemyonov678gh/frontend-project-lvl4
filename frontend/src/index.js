79b63e94d0aa13c4936753443fb3e006268f96bb
temasemyonov678gh <temasemyonov678@gmail.com>
2023-10-06 10:09:53 +0500

    add error component and edit not found page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,20 +13,47 @@ import {
   LEVEL_WARN,
 } from "@rollbar/react";
 import "react-toastify/dist/ReactToastify.css";
-import NotFound from "./pages/NotFound";
-
-const ErrorBoundaryPage = () => <NotFound />;
+import Alert from "react-bootstrap/Alert";
 
 const rollbarConfig = {
-  accessToken: process.env.REACT_APP_apiKey,
-  environment: process.env.REACT_APP_NODE_ENV,
+  enabled: process.env.REACT_APP_NODE_ENV === "production",
+  accessToken: process.env.REACT_APP_ROLLBAR_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  payload: {
+    environment: "production",
+    client: {
+      javascript: {
+        source_map_enabled: true,
+        code_version: "0.0.1",
+        guess_uncaught_frames: true,
+      },
+    },
+  },
 };
+
+const CustomError = () => (
+  <div className="error-section">
+    <Alert className="alert" key="danger" variant="danger">
+      <Alert.Heading className="alert-header">
+        Упс!.. Что-то пошло не так.
+      </Alert.Heading>
+      <p className="alert-body">
+        Извините, возникла ошибка на нашем сайте. Мы работаем над устранением
+        проблемы и приносим свои извинения за неудобства. Пожалуйста, повторите
+        попытку позже. Если ошибка продолжает возникать, пожалуйста, сообщите
+        нам о ней, чтобы мы могли быстро исправить ситуацию. Спасибо за ваше
+        терпение и понимание!
+      </p>
+    </Alert>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RollbarProvider config={rollbarConfig}>
-      <ErrorBoundary level={LEVEL_WARN} fallbackUI={ErrorBoundaryPage}>
+      <ErrorBoundary level={LEVEL_WARN} fallbackUI={CustomError}>
         <Provider store={store}>
           <App />
           <ToastContainer
--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,9 +1,26 @@
-import Header from '../components/Header.jsx';
+import { Link } from "react-router-dom";
+import Header from "../components/Header";
 
-function NotFound() {
-    return (
+const NotFound = () => {
+  return (
+    <div className="not-found-section">
       <Header />
-    );
-  }
-  
-  export default NotFound;
+      <div className="container">
+        <div className="error-container">
+          <h1>404</h1>
+          <div className="error-container-desctiption">
+            Упс!.. Страница не найдена.
+          </div>
+        </div>
+        <div className="link-container">
+          <Link to="/" className="link-home">
+            <img src={require("../assets/arrow.png")} />
+            На главную
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
